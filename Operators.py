c9aa4467fc3f9333a34135b32281849e380fb47a
adeel2910 <166474465+adeel2910@users.noreply.github.com>
2024-04-09 10:49:31 +0500

    my first commit

---
+++ Operators.py
@@ -0,0 +1,30 @@
+#a=5
+#b=3
+#print(a/b)
+#print(a//b)
+#print(a%b)
+#print(a**b)
+#print("hello"*3)
+#print(a==b)
+#s="a"
+#s2="b"
+#print(s<=s2)
+#a=a*10
+#a*=10
+#print(a)
+#a=True
+#b=False
+#print(True and True)
+#print(True and False)
+#print(False and True)
+#print(False and False)
+#print("..............")
+#print(True or True)
+#print(True or False)
+#print(False or True)
+#print(False or False)
+#print((10<11) and (not 9<10))
+l=[1,2,3,"a",True]
+print(l)
+print(1 not in l)
+print("a" in l)
---
+++ filehandlingdemo.py
@@ -0,0 +1,49 @@
+#from os import *
+import os
+#s="my demo file"
+#f=open("data.txt","w")
+#f.write(s)
+#f.close()
+#f=open("data.txt","r")
+#s=f.read()
+#print(s)
+#f.close()
+
+#with open("data.txt","r") as f:
+#print(f.read())
+
+#with open("data.txt","w") as f:
+ #   f.write("new content")
+
+
+#i=0
+#try:
+#    print(5/i)
+#    print("========")
+#    with open("data.txt", "r") as f:
+#        print(f.read())
+#except ZeroDivisionError:
+#    print("Run First exception Block")
+#except FileNotFoundError:
+#    print("Run Second Exception Block")
+#print("-------")
+
+
+#i=5
+#try:
+#    print(5/i)
+#   print("========")
+#except ZeroDivisionError:
+#    print("Run First exception Block")
+#finally:
+#    print("Run Finally")
+#print("-----")
+
+#print(listdir())
+#print(getcwd())
+
+#os.system("echo file.txt")
+#print(os.system("dir"))
+
+
+
